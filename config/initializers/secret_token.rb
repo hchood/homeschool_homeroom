c6433bf1b940e5712ae341a01273f750a01ea670
Helen Hood <helen.c.hood@gmail.com>
2014-01-11 10:42:31 -0500

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HomeschoolHomeroom::Application.config.secret_key_base = '0c9dc8ac96e41c896316b1a0a553ed6637b761e40465bc9c50c5776c487a22b02ea29dce6a7a367979d6d770b73bff35fbc5b3265c2b68862ab62ddf847163e4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+HomeschoolHomeroom::Application.config.session_store :cookie_store, key: '_homeschool_homeroom_session'
